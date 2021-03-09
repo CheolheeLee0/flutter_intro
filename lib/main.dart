22dc3889987177e81a351cdf97e1a12cb38461d6
CheolheeLee <wks0968@gmail.com>
2021-03-09 14:28:10 +0900

    Add app base

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
+import 'package:flutter_intro/constraints.dart';
+import 'package:flutter_intro/screens/home/components/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,7 +14,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
-    ),
+      debugShowCheckedModeBanner: false,
+      title: 'Plant App',
+      theme: ThemeData(
+        scaffoldBackgroundColor: kBackgroundColor,
+        primaryColor: kPrimaryColor,
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
   }
 }
