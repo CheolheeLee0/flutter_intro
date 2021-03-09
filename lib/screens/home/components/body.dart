7f9e52b46da16632d04ca0ed2988a6b53b4fe0ea
CheolheeLee <wks0968@gmail.com>
2021-03-09 16:02:38 +0900

    Add recommend_plants

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_intro/constraints.dart';
 import 'package:flutter_intro/screens/home/components/header_with_searchbox.dart';
+import 'package:flutter_intro/screens/home/components/recommend_plant_card.dart';
 import 'package:flutter_intro/screens/home/components/title_with_more_btn.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -13,11 +14,43 @@ class Body extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
+          // header with search box
           HeaderWithSearchBox(size: size),
+          // Recommended, More btn
           TitleWithMoreBtn(
             text: "Recommended",
             press: () {},
           ),
+
+          // 오른쪽으로 스크롤 이동
+          SingleChildScrollView(
+            // 수평 방향으로
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                // Images container,
+                RecommendPlantCard(
+                  image: "assets/images/image_1.png",
+                  title: "Samantha",
+                  country: "Russia",
+                  price: 440,
+                  press: () {},
+                ),
+                RecommendPlantCard(
+                  image: "assets/images/image_2.png",
+                  title: "Angelica",
+                  country: "Russia",
+                  price: 440,
+                ),
+                RecommendPlantCard(
+                  image: "assets/images/image_3.png",
+                  title: "Samantha",
+                  country: "Russia",
+                  price: 440,
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
